159cfa43b2205aa1f00826d3502cf7a99991389d
domiii <dominik.seifert@gmail.com>
2014-05-30 18:55:53 +0800

    - added Stacktrace & CodeBuilder to squishy namespace

--- index.js
+++ index.js
@@ -5,5 +5,7 @@
 "use strict";
 
 module.exports = require('./lib/squishy');
-module.exports.Stacktrace = require('./lib/squishy.Stacktrace');
-module.exports.CodeBuilder = require('./lib/squishy.CodeBuilder');
+
+// also attach Stacktrace and CodeBuilder to squishy for now (until we find a better place for them)
+squishy.Stacktrace = require('./lib/squishy.Stacktrace');
+squishy.CodeBuilder = require('./lib/squishy.CodeBuilder');
